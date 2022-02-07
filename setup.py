9f1e3814e033e765104860a81580e47e83250646
frmdstryr <frmdstryr@protonmail.com>
2022-02-07 14:23:30 -0500

    Update version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="atom-db",
-    version="0.7.3.dev",
+    version="0.7.3",
     author="CodeLV",
     author_email="frmdstryr@gmail.com",
     url="https://github.com/codelv/atom-db",
